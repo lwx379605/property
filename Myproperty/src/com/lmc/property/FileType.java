78b79a521a15ba6ca5a756c939294baa55f6afd4
lwx379605 limincheng <2545189721@qq.com>
2017-11-28 18:33:05 +0800

    2017/11/28/3

---
+++ Myproperty/src/com/lmc/property/FileType.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2005-2017 shopxx.net. All rights reserved.
+ * Support: http://www.shopxx.net
+ * License: http://www.shopxx.net/license
+ */
+package com.lmc.property;
+
+/**
+ * 文件类型
+ * 
+ * @author 李敏成
+ * 
+ */
+public enum FileType {
+
+	/**
+	 * 图片
+	 */
+	image,
+
+	/**
+	 * 媒体
+	 */
+	media,
+
+	/**
+	 * 文件
+	 */
+	file
+}
